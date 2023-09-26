787ef0256176bd17b77cad106298eea5d4e4acd9
danieltitton <tittondaniel@outlook.com>
2023-09-26 19:42:54 -0300

    desafio finalizado

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-console.log ("desafio");
+
+let NivelDoHeroi = "Imortal";
+let NomeDoHeroi = "Batman";
+let VidaDoHeroi = 9999;
+
+if (VidaDoHeroi < 1000) {
+    console.log(NivelDoHeroi = "Ferro ");
+} else if (VidaDoHeroi >= 1001 && VidaDoHeroi <= 2000) {
+    console.log(NivelDoHeroi = "Bronze");
+} else if (VidaDoHeroi >= 2001 && VidaDoHeroi <= 5000) {
+    console.log(NivelDoHeroi = "Prata");
+} else if (VidaDoHeroi >= 6001 && VidaDoHeroi <= 7000) {
+    console.log(NivelDoHeroi = "Ouro")
+} else if (VidaDoHeroi >= 7001 && VidaDoHeroi <= 8000) {
+    console.log(NivelDoHeroi = "Platina")
+} else if (VidaDoHeroi >= 8001 && VidaDoHeroi <= 9000) {
+    console.log(NivelDoHeroi = "Ascendente")
+} else if (VidaDoHeroi >= 9001 && VidaDoHeroi <= 10000) {
+    console.log(NivelDoHeroi = "Imortal")
+} else {
+    console.log(NivelDoHeroi = "Radiante")
+}
+
+console.log("O Heroi de nome ", (NomeDoHeroi), "esta no nivel ", (NivelDoHeroi));
